a603d0ef97be236b50d246f2ef31461ea45684b3
Yago Guirra <yagoguirra@hotmail.com>
2025-01-30 23:01:07 -0300

    first commit

---
+++ include/led.h
@@ -0,0 +1,10 @@
+#ifndef LED_H
+#define LED_H
+
+#define LED_RED_PIN     13
+#define LED_YELLOW_PIN  12
+#define LED_GREEN_PIN   11
+
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#include "include/led.h"
+
+
+
+int main() {
+    stdio_init_all();
+    
+}
