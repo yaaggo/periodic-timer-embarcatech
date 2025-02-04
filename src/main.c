bf2a82ed13fdf29b79c610b0bde7700046954c0d
Yago Guirra <yagoguirra@hotmail.com>
2025-02-04 11:52:49 -0300

    comentando main.c

--- src/main.c
+++ src/main.c
@@ -4,24 +4,29 @@
 
 #include "include/led.h"
 
+// varaivel para controlar o indice da rotina de leds
 volatile uint8_t led_index = 0;
 
+// rotina
 uint8_t leds_routine[] = {
     LED_RED_PIN,
     LED_YELLOW_PIN,
     LED_GREEN_PIN
 };
 
+// cores para poder fazer o printf
 char *cores[] = {
     "vermelho",
     "amarelo",
     "verde"
 };
 
+// função de callback
 bool traffic_lights_routine(struct repeating_timer *t) {  
     printf("Luz %s desligou\n", cores[led_index]);
     led_state(leds_routine[led_index], false);
 
+    // pega o indice certo para o caso em que sai dos limites do vetor
     led_index = led_index >= 2 ? 0 : led_index + 1;
     
     printf("Luz %s ligou" 
@@ -38,11 +43,12 @@ int main() {
     struct repeating_timer timer;
     
     stdio_init_all();
-    
+    // inicializando os leds
     led_init(LED_RED_PIN);
     led_init(LED_YELLOW_PIN);
     led_init(LED_GREEN_PIN);
 
+    // faz o temporizador periódico
     add_repeating_timer_ms(
         3000, 
         traffic_lights_routine, 
@@ -50,10 +56,12 @@ int main() {
         &timer
     );
 
+    // começa com o led vermelho ligado
     led_state(LED_RED_PIN, true);
 
+    // acumulador de tempo para sincronizar os printf's
     uint16_t time_acm = 0;
-
+        
     while (true) {
         sleep_ms(1000);
         time_acm += 1000;
